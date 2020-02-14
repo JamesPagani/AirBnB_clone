47bcedcc6ab1e1c1606b197e0a92c540f5cc4baf
JamesPagani <jaimeagalvezv@gmail.com>
2020-02-14 18:35:22 +0000

    Parsed created_at and updated_at to datetime format

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,35 @@ from uuid import uuid4
 class BaseModel:
     """Template for all other classes."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new class object."""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None:
+            self.__dict__.update(kwargs)
+            if "created_at" in self.__dict__:
+                self.created_at = self.created_at.split(sep="T")
+                self.created_at.extend(self.created_at.pop(0).split(sep="-"))
+                self.created_at.extend(self.created_at.pop(0).split(sep=":"))
+                self.created_at.extend(self.created_at.pop().split(sep="."))
+                for i in range(len(self.created_at)):
+                    self.created_at[i] = int(self.created_at[i])
+                self.created_at = datetime(*self.created_at)
+
+            if "updated_at" in self.__dict__:
+                self.updated_at = self.updated_at.split(sep="T")
+                self.updated_at.extend(self.created_at.pop(0).split(sep="-"))
+                self.updated_at.extend(self.created_at.pop(0).split(sep=":"))
+                self.updated_at.extend(self.created_at.pop().split(sep="."))
+                for i in range(len(self.updated_at)):
+                    self.updated_at[i] = int(self.updated_at)
+                self.updated_at = datetime(*self.updated_at)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the object."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,\
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
 
     def save(self):
