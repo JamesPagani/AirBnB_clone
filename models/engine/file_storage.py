1346cb72b47759895f728eb8711dfbc065a40ada
Cybernuki <1164@holbertonschool.com>
2020-02-17 17:52:46 -0500

    Fix somenthing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,12 @@
 """This module contains the engine of persistence"""
 import json
 from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage():
